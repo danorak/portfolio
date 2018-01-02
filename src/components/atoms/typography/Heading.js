b69338a16ca29cdd8a26a103fe7d85924cf720cf
Dan Jones <danorak@Dans-MBP.local>
2018-01-02 21:10:37 +0000

    first commit

---
+++ src/components/atoms/typography/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+    font-size: 49px;
+    line-height: 1.3;
+    font-weight: 600;
+    color: #333333;
+`;
+
+export default Heading;
---
+++ src/components/atoms/typography/Intro.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Intro = styled.p`
+    font-size: 32px;
+    line-height: 1.3;
+    color: #444444;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
+
+export default Intro;
